6f9cf106738c93813be338cd6a1e9443e93524de
Dmitriy Boyko <dimaboyko594@gmail.com>
2022-11-06 23:25:29 +0300

    first day

--- src/by/boyko/helperapp/ConsoleApp.java
+++ src/by/boyko/helperapp/ConsoleApp.java
@@ -1,9 +1,50 @@
 package by.boyko.helperapp;
 
+import java.io.File;
+import java.util.List;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class ConsoleApp {
+    private String homePath;
+
     public void setup() {
 
     }
 
+    public ConsoleApp() {
 
+    }
+
+    public List<Path> findByFolderName(String folderName) throws IOException {
+        String pathStart = "D:\\";
+        List<Path> results = null;
+
+//        try (Stream<Path> pathStream = Files.find(Paths.get(pathStart), Integer.MAX_VALUE, (p, basicFileAttributes) -> {
+//            if (Files.isRegularFile(p) || !Files.isReadable(p) || !Files.isWritable(p)) {
+//                return false;
+//            }
+//
+//            return p.endsWith(folderName);
+//        }, FileVisitOption.FOLLOW_LINKS)) {
+//            results = pathStream.collect(Collectors.toList());
+//        } catch (AccessDeniedException ade) {
+//            System.out.println("файлик не читаица :((");
+//        }
+
+        try (Stream<Path> walk = Files.walk(Paths.get(pathStart))) {
+            results = walk.filter(Files::isDirectory)
+                    .map(Path::toFile)
+                    .filter(File::canWrite)
+                    .map(File::toPath)
+                    .collect(Collectors.toList());
+        }
+
+        return results;
+    }
 }
+
--- src/by/boyko/helperapp/ConsoleHelper.java
+++ src/by/boyko/helperapp/ConsoleHelper.java
@@ -1,9 +1,41 @@
 package by.boyko.helperapp;
 
+import java.io.IOException;
+import java.nio.file.AccessDeniedException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
 import java.util.Scanner;
+import java.util.stream.Stream;
+import java.util.List;
+
+/*
+* В первую очередь нужно сделать так, чтобы пользователь вводил путь и
+* */
 
 public class ConsoleHelper {
     public static void main(String[] args) {
+        Path path;
         Scanner scanner = new Scanner(System.in);
+        String[] defaultPaths = {"C:\\", "D:\\"};
+        System.out.println("Введите название начальной папки");
+        String p = scanner.next();
+        // пользователь вводит имя папки, в которой будет произведена работа (Desktop)
+        if (p == "") {
+            path = Paths.get("C:\\");
+        }
+
+        ConsoleApp app = new ConsoleApp();
+        List<Path> paths = new ArrayList<>();
+
+        try {
+            paths = app.findByFolderName(p);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        System.out.println(paths);
     }
 }
