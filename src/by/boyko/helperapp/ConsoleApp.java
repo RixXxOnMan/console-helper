685b5b6937166644c8163d60cb28c6afd648c551
Dmitriy Boyko <dimaboyko594@gmail.com>
2022-11-06 17:32:33 +0300

    starting project

---
+++ src/by/boyko/helperapp/ConsoleApp.java
@@ -0,0 +1,9 @@
+package by.boyko.helperapp;
+
+public class ConsoleApp {
+    public void setup() {
+
+    }
+
+
+}
---
+++ src/by/boyko/helperapp/ConsoleHelper.java
@@ -0,0 +1,9 @@
+package by.boyko.helperapp;
+
+import java.util.Scanner;
+
+public class ConsoleHelper {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
